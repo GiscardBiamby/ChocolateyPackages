c00fad6e8d145ebea4505c27718c32cce0c0bb8e
GiscardBiamby <giscardo@yahoo.com>
2013-02-27 09:35:36 -0500

    Initial commit; rando scripts for setting up my workstation.

---
+++ KillCassini/KillCassini.ps1
@@ -0,0 +1,8 @@
+Stop-Process -ProcessName WebDev.WebServer40* -Verbose 
+
+#    Try both x86 and x64 versions. If I weren't lazy i'd 
+#    put in a check for the bitness of the current system. 
+autoit3 C:\users\giscard\downloads\_RefreshNotificationAreaIcons.au3
+autoit3_x64 C:\users\giscard\downloads\_RefreshNotificationAreaIcons.au3
+
+
---
+++ SrWare_BlockList.ps1
@@ -0,0 +1,13 @@
+﻿$source = "http://www.fanboy.co.nz/adblock/opera/complete/urlfilter.ini"
+$destination = "C:/Program Files (x86)/SRWare Iron/adblock.ini"
+
+$newValue = (new-object net.webclient).DownloadString($source).Replace('*','').Replace('#',' ')
+
+if ($newValue -gt '') {
+    Remove-Item -Path $destination 
+    Add-Content -Path $destination -Value $newValue
+}
+
+
+
+

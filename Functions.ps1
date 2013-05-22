9f887b8f5630b0b85346fa74b025b5c21f954f11
GiscardBiamby <giscardo@yahoo.com>
2013-05-22 08:32:51 -0400

    Updated the restart-vpn function to kill the ShrewSoft vpn client process if it is running.

--- Functions.ps1
+++ Functions.ps1
@@ -1,4 +1,12 @@
 function Restart-Vpn {
+	$vpnClientProcess = Get-Process -Name ipsecc -ErrorAction SilentlyContinue
+	if ($vpnClientProcess -eq $null) {
+		Write-Host "Vpn client is not running."
+	} else {
+		Write-Host "Stopping vpn client process."
+		Stop-Process -Id $vpnClientProcess.Id
+	}
+	
 	Restart-Service ipsecd
 	Restart-Service iked
 }
